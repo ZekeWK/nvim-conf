f2a6be3b6756b51525e3b5ea3c5da146f5ed7fb9
William Kraft <william.z.kraft@gmail.com>
2023-07-19 14:57:41 +0200

    Long overdue

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-package.path = package.path .. ";/home/wk/code/proj/nvim/lua/?.lua"
 for _, source in ipairs{
 	'opts',
 	'packer-conf',
@@ -6,6 +5,7 @@ for _, source in ipairs{
 	'cmp-conf',
 	'treesitter-conf',
 	'luasnip-conf',
+	'vimtex-conf',
 } do
 	local ok, val = pcall(require, source)
 	if ok then else vim.api.nvim_err_writeln('Error at: ' .. source) end
